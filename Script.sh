bcf42a643e32247993c6a140ea0552d843746115
suhasburge <suhasburge07@gmail.com>
2026-02-20 17:07:41 +0530

    First Script

---
+++ Script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is my first Script
+
+echo "Teacher: So Students today we will learn new topic"
+
+echo "Student: What is the topic name?"
+
+echo "Teacher: Ethical Hacking"
+
+echo "Student: Ok"
